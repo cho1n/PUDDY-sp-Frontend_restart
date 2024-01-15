9b792427d1bfb507fad15a628b12d62334f76adc
cho1n <cjaewon0217@naver.com>
2024-01-15 16:43:42 +0900

    Desgin: Global Style 및 색상, fontSize 등 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <p className="text-5xl font-bold underline bg-gray-400 mb-3" > Start Puddy </p>
-        <button className="bg-yellow-300 w-500 h-500"onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <p className="text-5xl text-fontBlack font-bold mb-3"> Logo </p>
+        <button className="bg-bgYellow w-500 h-500">count is</button>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fontGreen: "#0F940D",
+        fontRed: "#FF0000",
+        fontGray: "#6F6D6D",
+        fontBlack: "#000000",
+        fontWhite: "#FFFFFF",
+      },
+      backgroundColor: {
+        bgYellow: "#FFDD2E",
+        bgRed: "#FF3131",
+        bgGray: "#E6E0E9",
+        bgBlack: "#000000",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      fontSize: {
+        bigTitle: "22px",
+        middleTitle: "28px",
+        defalut: "14px",
+        buttonFont: "20px",
+        smallTitle: "14px",
+      },
+      
+    },
   },
   plugins: [],
 };
