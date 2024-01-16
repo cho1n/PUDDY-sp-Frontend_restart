cf785f036974f292ff731f2ef42f6f655b345d31
JeongHeumChoi <andyc707@gmail.com>
2024-01-16 20:43:24 +0900

    feat: Header 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { FooterNav } from "./components/common/FooterNav/FooterNav.tsx";
+import { Header } from "./components/common/Header/header.tsx";
 
 function App() {
   return (
     <>
+      <Header title={"마이페이지"} />
       <p className="text-5xl text-fontBlack font-bold mb-3"> Logo </p>
       <button className="bg-bgYellow w-500 h-500">count is</button>
       <FooterNav />
