1b37e62c360747b477142e6f815e0a18f516e599
JeongHeumChoi <andyc707@gmail.com>
2024-01-18 19:10:02 +0900

    feat: 뒤로 가기 기능 구현

--- src/components/common/Header/header.tsx
+++ src/components/common/Header/header.tsx
@@ -5,12 +5,19 @@ interface Title {
 }
 
 export const Header = (titleObject: Title) => {
+  const handleBack = () => {
+    window.history.back();
+  };
+
   return (
     <div className="flex w-full h-16 px-1 py-2 items-center gap-1 shrink-0">
       <div className="flex justify-center items-center gap-2.5 ">
-        <a href="" className="flex p-2 justify-center items-center gap-2.5">
+        <div
+          className="flex p-2 justify-center items-center gap-2.5"
+          onClick={() => handleBack()}
+        >
           <img src={arrow} className="w-6 h-6" alt="arrow" />
-        </a>
+        </div>
       </div>
       <p className="flex-1 h-6 leading-7 font-normal text-2xl text-left">
         {titleObject.title}
