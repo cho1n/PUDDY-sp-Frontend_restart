ec6cee33cf071da51b0c74c64cfc8d68bb7c16ef
JeongHeumChoi <andyc707@gmail.com>
2024-01-18 19:05:29 +0900

    fix: navMode 초기화 상수 설정

--- src/store/useNavstore.tsx
+++ src/store/useNavstore.tsx
@@ -1,5 +1,6 @@
 import create from "zustand";
 import { persist } from "zustand/middleware";
+import { NAV_MODE } from "../constants/footerNav.ts";
 
 interface NavStore {
   navMode: string;
@@ -9,7 +10,7 @@ interface NavStore {
 const useNavStore = create(
   persist<NavStore>(
     (set) => ({
-      navMode: "0",
+      navMode: NAV_MODE.HOME,
       selectMode: (mode: string) => {
         set({ navMode: mode });
       },
