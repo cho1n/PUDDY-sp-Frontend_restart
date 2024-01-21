1d6f5942c728b41f4341d50cfe97711cc587e210
JeongHeumChoi <andyc707@gmail.com>
2024-01-22 02:39:30 +0900

    feat: 내 주변 산책로 페이지 연결

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SignInPage, SignUpPage, PostDogPage } from "../pages";
+import { RecommendWalkRoadPage } from "../pages/RecommendWalkRoadPage.tsx";
 export const AppRouter = () => {
   return (
     <BrowserRouter>
@@ -7,6 +8,7 @@ export const AppRouter = () => {
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/postdog" element={<PostDogPage />} />
+        <Route path="/find-walk-road" element={<RecommendWalkRoadPage />} />
       </Routes>
     </BrowserRouter>
   );
