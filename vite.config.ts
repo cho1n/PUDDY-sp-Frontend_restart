4d0929db2376daaefb4774c50d15ed69ccb6f998
cho1n <cjaewon0217@naver.com>
2024-02-13 17:35:04 +0900

    Fix: Vite config 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: {},
-  },
   plugins: [react()],
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
 });
